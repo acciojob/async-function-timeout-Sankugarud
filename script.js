1e6fbf060c250a7f24ebabac6411df9610dfc9b2
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-29 00:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById('btn');
 btn.addEventListener('click', callme);
-
+cy.get("div#output").then(txt => {
+  expect(txt.text().trim()).to.equal("Test - 2");
+});
 async function callme() {
   let textInput = document.getElementById('text');
   let inputValue = textInput.value;
