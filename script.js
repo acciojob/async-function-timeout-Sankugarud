cb8b831b036fb2fc6bdf6e15bc3941ce18ce0c79
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-29 00:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', callme);
+
+
+
+async function callme(){
+	let text = document.getElementById('text');
+	let inputValue = text.value;
+
+	
+
+	let number = document.getElementById('delay');
+		let numberValue = number.value;
+	
+	await new Promise(resolve => {
+    setTimeout(resolve, numberValue * 1000);
+  });
+		let output = document.getElementById('output');
+			output.innerHTML = inputValue;
+
+}
