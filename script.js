a8db540f9c6b0d9de80553da9ed7f9769e09fd5e
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-29 00:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-//your JS code here. If required.
-
 let btn = document.getElementById('btn');
 btn.addEventListener('click', callme);
 
+async function callme() {
+  let textInput = document.getElementById('text');
+  let inputValue = textInput.value;
 
+  let numberInput = document.getElementById('delay');
+  let numberValue = numberInput.value;
 
-async function callme(){
-	let text = document.getElementById('text');
-	let inputValue = text.value;
-
-	
-
-	let number = document.getElementById('delay');
-		let numberValue = number.value;
-	
-	await new Promise(resolve => {
+  await new Promise(resolve => {
     setTimeout(resolve, numberValue * 1000);
   });
-		let output = document.getElementById('output');
-			output.innerHTML = inputValue;
 
+  let output = document.getElementById('output');
+  output.innerHTML = inputValue;
 }
